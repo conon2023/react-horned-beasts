93799d558230469b58f5fc59a3d57407bee331d4
CHIDI ONONYE <fononye2020@gmail.com>
2023-07-21 04:05:06 +0100

    update horned beasts dynamically using the data.jason file provided

--- src/components/Horned-Beasts.jsx
+++ src/components/Horned-Beasts.jsx
@@ -1,25 +1,12 @@
 import React from "react";
 
-function HornedBeasts() {
+function HornedBeasts({ title, image, description }) {
   return (
     <div className="main-content">
-        <div>
-      <h2>RHINOS</h2>
-      <img src="https://images.unsplash.com/photo-1547970810-dc1eac37d174?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=636&q=80" alt="imagebeasts" className="image-size"></img>
-      <h3>This are Rhinos. They walk like kings and queens. 
-      </h3>
-      </div>
       <div>
-      <h2>ELEPHANTS</h2>
-      <img src="https://images.unsplash.com/photo-1578326626553-39f72c545b07?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=498&q=80" alt="imagebeasts" className="image-size"></img>
-      <h3>These are elephants. They are kings in the jungle. 
-      </h3>
-      </div>
-      <div>
-      <h2>CHEETAHS</h2>
-      <img src="https://images.unsplash.com/photo-1576222901803-c062b57925b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=551&q=80" alt="image-beasts" className="image-size"></img>
-      <h3>These are Cheetahs. They run very fast.
-      </h3>
+        <h2>{title}</h2>
+        <img src={image} alt={description} className="image-size"></img>
+        <h3>{description}</h3>
       </div>
     </div>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,10 +2,30 @@ import React from "react";
 import HornedBeasts from "./Horned-Beasts";
 import Data from "../Data.json";
 
-function Main(props) {
+function Main() {
   return (
     <main>
-      <HornedBeasts />
+      <HornedBeasts title = {Data[0].title} image = {Data[0].image_url} description = {Data[0].description}/>
+      <HornedBeasts title = {Data[1].title} image = {Data[1].image_url} description = {Data[1].description}/>
+      <HornedBeasts title = {Data[2].title} image = {Data[2].image_url} description = {Data[2].description}/>
+      <HornedBeasts title = {Data[3].title} image = {Data[3].image_url} description = {Data[3].description}/>
+      <HornedBeasts title = {Data[4].title} image = {Data[4].image_url} description = {Data[4].description}/>
+      <HornedBeasts title = {Data[5].title} image = {Data[5].image_url} description = {Data[5].description}/>
+      <HornedBeasts title = {Data[6].title} image = {Data[6].image_url} description = {Data[6].description}/>
+      <HornedBeasts title = {Data[7].title} image = {Data[7].image_url} description = {Data[7].description}/>
+      <HornedBeasts title = {Data[8].title} image = {Data[8].image_url} description = {Data[8].description}/>
+      <HornedBeasts title = {Data[9].title} image = {Data[9].image_url} description = {Data[9].description}/>
+      <HornedBeasts title = {Data[10].title} image = {Data[10].image_url} description = {Data[10].description}/>
+      <HornedBeasts title = {Data[11].title} image = {Data[11].image_url} description = {Data[11].description}/>
+      <HornedBeasts title = {Data[12].title} image = {Data[12].image_url} description = {Data[12].description}/>
+      <HornedBeasts title = {Data[13].title} image = {Data[13].image_url} description = {Data[13].description}/>
+      <HornedBeasts title = {Data[14].title} image = {Data[14].image_url} description = {Data[14].description}/>
+      <HornedBeasts title = {Data[15].title} image = {Data[15].image_url} description = {Data[15].description}/>
+      <HornedBeasts title = {Data[16].title} image = {Data[16].image_url} description = {Data[16].description}/>
+      <HornedBeasts title = {Data[17].title} image = {Data[17].image_url} description = {Data[17].description}/>
+      <HornedBeasts title = {Data[18].title} image = {Data[18].image_url} description = {Data[18].description}/>
+      <HornedBeasts title = {Data[19].title} image = {Data[19].image_url} description = {Data[19].description}/>
+
     </main>
   );
 }
