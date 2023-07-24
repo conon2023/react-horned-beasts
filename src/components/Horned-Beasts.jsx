be2ea7b241307252b02cf5ccc7430c68ab38b9f6
CHIDI ONONYE <fononye2020@gmail.com>
2023-07-24 06:40:34 +0100

    updated Horned beast for users to be able to click on favorite image

--- src/components/Horned-Beasts.jsx
+++ src/components/Horned-Beasts.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import '../App.css'
 
-function HornedBeasts({ title, image, description }) {
+
+export default function HornedBeasts({ title, image_url, description }) {
+  const [fav, setfav] = useState (0);
+
+  function handleFav (){
+     setfav(fav + 1);
+  }
   return (
     <div className="parent">
       <div>
         <h2>{title}</h2>
-        <img src={image} alt={description} className="image-size"></img>
+        <img src={image_url} alt={title} className="image-size"></img>
         <h3>{description}</h3>
       </div>
+      <p>
+        <span className="heart" onClick={handleFav}>
+        ❤️
+        </span>
+      </p>
     </div>
   );
 }
 
-export default HornedBeasts;
+  
